1e6a4218da75d07ce638affc269fe6d8a331188a
RizaldyAditya <rizaldy.aditya.n@gmail.com>
2026-09-18 11:22:39 +0700

    fix(crawl): use integer episode number instead of display string
    - Change $episodeNumber to $episodeNumberPre across all crawl observers
    - Fixes MySQL Data truncated error on episode_number int column

--- app/Observers/DonghuaworldIndexObserver.php
+++ app/Observers/DonghuaworldIndexObserver.php
@@ -140,7 +140,7 @@ class DonghuaworldIndexObserver extends CrawlObserver
                             'donghua_id' => $this->donghuaId,
                             'title' => trim($episodeTitle),
                             'stream_id' => $stream->id,
-                            'episode_number' => $episodeNumber,
+                            'episode_number' => $episodeNumberPre,
                             'video_source_url' => $videoSourceUrlJson,
                             'is_an_update' => false,
                         ]
--- app/Observers/DonghuazoneObserver.php
+++ app/Observers/DonghuazoneObserver.php
@@ -129,7 +129,7 @@ class DonghuazoneObserver extends CrawlObserver
                                 'donghua_id' => $donghua_id,
                                 'title' => trim($episodeTitle),
                                 'stream_id' => $stream->id,
-                                'episode_number' => $episodeNumber,
+                                'episode_number' => $episodeNumberPre,
                                 'video_source_url' => $videoSourceUrlJson,
                             ]
                         );
